c996be8692a1594806d0afff61d4030d1190476b
NameGill <gillarong@163.com>
2022-05-19 17:08:19 +0800

    DjangoTest

---
+++ socket_server.py
@@ -0,0 +1,45 @@
+import socket
+import time
+
+
+EOL1 = b'\n\n'
+EOL2 = b'\n\r\n'
+body = '''Hello! <h1> from <<Django>> <h1>'''
+response_params = [
+    'HTTP/1.0 200 OK',
+    'Date: Sun,11 Oct 2022 01:01:01 GMT',
+    'Content-Type: text/html; charset=utf-8',
+    'Content-Length: {}\r\n'.format(len(body.encode())),
+    body,
+]
+response = '\r\n'.join(response_params)
+
+
+def handle_connection(conn, addr):
+    print('oh, new conn', conn, addr)
+    time.sleep(100)
+    request = b""
+    while EOL1 not in request and EOL2 not in request:
+        request += conn.recv(1024)
+    print(request)
+    conn.send(response.encode())
+    conn.close()
+
+
+def main():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serversocket.bind(('127.0.0.1', 8000))
+    serversocket.listen(5)
+    print('http://127.0.0.1:8000')
+
+    try:
+        while True:
+            conn, address = serversocket.accept()
+            handle_connection(conn, address)
+    finally:
+        serversocket.close()
+
+
+if __name__ == '__main__':
+    main()
