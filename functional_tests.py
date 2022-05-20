373f243f9e2552fc5eacb2d8c325c59ae5941b24
NameGill <gillarong@163.com>
2022-05-20 10:20:21 +0800

    change the test code

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        """
+        测试方法之前；若setUp异常，tearDown方法不会执行
+        """
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """
+        测试方法之后；就算测试中出错，也会运行tearDown。即Firefox窗口不会一直停留在桌面上
+        """
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """
+        以test开头的均为测试方法
+        """
+        self.browser.get('http://localhost:8000')
+        self.assertIn('successfully', self.browser.title)  # unittest的辅助函数（断言）
+        self.fail('Finish the test!')  # 生成指定错误消息，提示测试结束
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')  # 禁止抛出ResourceWarning异常
